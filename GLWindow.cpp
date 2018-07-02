47c0939b7eea983df77b333c1d9e9d1216982630
tianyahechy <tianyahechu2004@163.com>
2018-07-02 15:23:53 +0800

    加上顶点着色器加上白点

--- GLWindow.cpp
+++ GLWindow.cpp
@@ -1,10 +1,20 @@
 #include "GLWindow.h"
+#include <qfile.h>
+#include <qsurfaceformat.h>
+#include <qopenglcontext.h>
 
 GLWindow::GLWindow(QWindow *parent)
 	: QWindow(parent)
 {
 	this->setSurfaceType(OpenGLSurface);
 	this->resize(640, 360);
+	g_vertexShader = 0;
+	g_FragmentShader = 0;
+	g_Program = 0;
+	g_VertexArray = 0;
+	_strVertexFileName = "Shader.vert";
+	_strFragFileName = "Shader.frag";
+	m_pContext = Q_NULLPTR;
 }
 
 GLWindow::~GLWindow()
@@ -12,10 +22,22 @@ GLWindow::~GLWindow()
 
 }
 
+QByteArray GLWindow::getShaderDataFromFile(const QString& fileName)
+{
+	QByteArray result;
+	QFile shaderFile(fileName);
+	if (shaderFile.open(QIODevice::ReadOnly))
+	{
+		result = shaderFile.readAll();
+		shaderFile.close();
+	}
+	return result;
+}
+
 void GLWindow::exposeEvent(QExposeEvent * pEvent)
 {
 	Q_UNUSED(pEvent);
-	if ( m_pContext == Q_NULLPTR )
+	if (m_pContext == Q_NULLPTR)
 	{
 		QSurfaceFormat format;
 		format.setVersion(4, 3);//需要opengl4.3版本
@@ -28,25 +50,66 @@ void GLWindow::exposeEvent(QExposeEvent * pEvent)
 		this->initializeOpenGLFunctions();
 		this->InitGL();
 	}
-	this->resize(this->width(), this->height());
+	this->ResizeGL(this->width(), this->height());
 	this->RenderGL();
 	m_pContext->swapBuffers(this);
 }
 
-
 void GLWindow::InitGL()
 {
+	//初始化顶点着色器
+	QByteArray vsData = this->getShaderDataFromFile(_strVertexFileName.c_str());
+	const char * vsPointer = vsData.constData();
+	g_vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(g_vertexShader,	//着色器标识
+		1,				//着色器文本数目
+		&vsPointer,		//着色器字符串指针
+		Q_NULLPTR		//字符串长度	
+		);
+	glCompileShader(g_vertexShader);
 
+	//初始化片元着色器
+	QByteArray fsData = this->getShaderDataFromFile(_strFragFileName.c_str());
+	const char * fsPointer = fsData.constData();
+	g_FragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(g_FragmentShader,	//着色器标识
+		1,				//着色器文本数目
+		&fsPointer,		//着色器字符串指针
+		Q_NULLPTR		//字符串长度	
+		);
+	glCompileShader(g_FragmentShader);
+
+	//初始化着色器程序
+	g_Program = glCreateProgram();
+	glAttachShader(g_Program, g_vertexShader);
+	glAttachShader(g_Program, g_FragmentShader);
+	glLinkProgram(g_Program);
+
+	//删除着色器
+	glDeleteShader(g_vertexShader);
+	glDeleteShader(g_FragmentShader);
+
+	//初始化顶点数组对象
+	glGenVertexArrays(1, &g_VertexArray);
+	glBindVertexArray(g_VertexArray);
+	glPointSize(10.0f);
 }
 
 void GLWindow::ResizeGL(int width, int height)
 {
-	Q_UNUSED(width);
-	Q_UNUSED(height);
+	glViewport(0, 0, width, height);
 }
 
 void GLWindow::RenderGL()
 {
-	static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-	glClearBufferfv(GL_COLOR, 0, red);
+	static const GLfloat black[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	glClearBufferfv(GL_COLOR, 0, black);
+	glUseProgram(g_Program);
+	glDrawArrays(GL_POINTS, 0, 1);
+}
+
+void GLWindow::ReleaseGL()
+{
+	glDeleteVertexArrays(1, &g_VertexArray);
+	glDeleteProgram(g_Program);
 }
--- GLWindow.h
+++ GLWindow.h
@@ -16,9 +16,24 @@ protected:
 	void InitGL();	//额外Opengl的初始化
 	void RenderGL();//OpenGL渲染相关
 	void ResizeGL(int width, int height); //窗口大小改变时
-
+	void ReleaseGL();	//释放资源
+	void onClose(void);
 private:
 	QOpenGLContext * m_pContext;
+
+private:
+	GLuint g_vertexShader;
+	GLuint g_FragmentShader;
+	GLuint g_Program;
+	GLuint g_VertexArray;
+
+public:
+	//从文件获取shader
+	QByteArray getShaderDataFromFile(const QString& fileName);
+	//shader文件名 
+	std::string _strVertexFileName;		//顶点着色器
+	std::string _strFragFileName;		//片元着色器
+
 	
 };
 
